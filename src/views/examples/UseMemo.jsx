c171c9d751d748d7bd54e8c2bbcda1e49eec1dfa
guisantosalves <workguisantos@gmail.com>
2022-04-29 14:39:48 -0400

    useMemo

--- src/views/examples/UseMemo.jsx
+++ src/views/examples/UseMemo.jsx
@@ -1,20 +1,26 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import PageTitle from "../../components/layout/PageTitle";
 import SectionTitle from "../../components/layout/SectionTitle";
 
 function sum(num1, num2){
     var n1 = parseInt(num1)
     var n2 = parseInt(num2)
+    const future = Date.now() + 2000;
+    while(Date.now() < future){} //espera 2s
     return n1 + n2
 }
 
 
 const UseMemo = (props) => {
+
   const [n1, setN1] = useState(0);
   const [n2, setN2] = useState(0);
   const [n3, setN3] = useState(0);
 
-  const result = sum(n1, n2)
+  //usando useMemo
+  //ele so vai fazer a operacao de soma quando o valor de n1 ou n2 for alterado
+  const result = useMemo(() => sum(n1, n2), [n1, n2])
+
   return (
     <div className="UseMemo">
       <PageTitle title="Hook UseMemo" subtitle="Retorna um valor memoizado!" />
