38f8de1a92a330d4e37f6cdc8cfcf93c4e508a80
guisantosalves <workguisantos@gmail.com>
2022-04-29 12:25:09 -0400

    useEffect

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -1,15 +1,54 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useEffect, useState } from "react";
+import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
 
-const UseEffect = (props) => {
-    return (
-        <div className="UseEffect">
-            <PageTitle
-                title="Hook UseEffect"
-                subtitle="Permite executar efeitos colaterais em componentes funcionais!"
-            />
-        </div>
-    )
+function calcFatorial(num){
+    const n = parseInt(num)
+
+    if(n<0) return -1
+    if(n===0) return 1
+    return calcFatorial(n-1) * n
+    
 }
 
-export default UseEffect
+const UseEffect = (props) => {
+  const [number, setNumber] = useState(1);
+  const [fatorial, setFatorial] = useState(1);
+
+  //dentro do [] no segundo parâmetro vai a dependência para a função ser chamada
+  // sempre que number mudar vai ser chamado o useEffect
+  useEffect(()=>{
+    setFatorial(calcFatorial(number))
+  }, [number])  
+
+  return (
+    <div className="UseEffect">
+      <PageTitle
+        title="Hook UseEffect"
+        subtitle="Permite executar efeitos colaterais em componentes funcionais!"
+      />
+      <SectionTitle title="Exercicio 01"/>
+      <div className="center">
+        <div>
+          <span className="text">Fatorial: </span>
+          <span className="text red">{fatorial === -1 ? "Não existe" : fatorial}</span>
+        </div>
+
+        <input
+          type="number"
+          className="input"
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+        />
+        <span className="text">{number}</span>
+      </div>
+
+      <SectionTitle title="Exercicio 02"/>
+      <div className="center">
+        
+      </div>
+    </div>
+  );
+};
+
+export default UseEffect;
